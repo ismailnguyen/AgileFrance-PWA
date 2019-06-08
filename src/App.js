2c9c522ed6695bb9c0858784be29f5e8c0e4f6a2
Ismaïl <nguyen.ismail@gmail.com>
2019-06-08 21:08:49 +0200

    Update home page design

--- src/App.js
+++ src/App.js
@@ -1,26 +1,95 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './logo.png';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to aareload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Talk from './Talk/Talk';
+import TalkDetailModal from './Talk/TalkDetailModal';
+import DayScheduleNavigation from './DayScheduleNavigation/DayScheduleNavigation'
+import scheduleDatas from './datas/schedule.json';
+
+
+class App extends React.Component {
+  constructor()  {
+    super();
+
+    this.state = {
+      schedule: scheduleDatas
+    };
+  }
+
+  eventsForHalfdayTime(dayName, halfdayName) {
+    return this.state.schedule.days.find(d => d.dayName === dayName).events
+        .filter(e => e.halfdayName.toLowerCase().includes(halfdayName.toLowerCase()));
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="header">
+          <img src={logo} className="logo" alt="logo" />
+          <h4>La Grande Conférence Agile Francophone</h4>
+          <small>Chalet de la Porte Jaune - Bois de Vincennes, Paris</small>
+        </header>
+
+        <div className="container main-container">
+
+          <DayScheduleNavigation>
+            <div label="Jeudi">
+                <div className="row title-row">
+                <div className="col-xs-12">
+                  <h3>Matin</h3>
+                </div>
+              </div>
+              <div className="row">
+              { this.eventsForHalfdayTime('Jeudi', 'Matin').map((item, index) =>
+                  <Talk data={ item } key={index} />
+                ) }
+              </div>
+              
+              <div className="row title-row">
+                <div className="col-xs-12">
+                  <h3>Apr&egrave;s midi</h3>
+                </div>
+              </div>
+              <div className="row">
+              { this.eventsForHalfdayTime('Jeudi', 'Midi').map((item, index) =>
+                  <Talk data={ item } key={index} />
+                ) }
+              </div>
+            </div>
+            <div label="Vendredi">
+                <div className="row title-row">
+                <div className="col-xs-12">
+                  <h3>Matin</h3>
+                </div>
+              </div>
+              <div className="row">
+              { this.eventsForHalfdayTime('Vendredi', 'Matin').map((item, index) =>
+                  <Talk data={ item } key={index} />
+                ) }
+              </div>
+              
+              <div className="row title-row">
+              <div className="col-xs-12">
+                  <h3>Apr&egrave;s midi</h3>
+                </div>
+              </div>
+              <div className="row">
+              { this.eventsForHalfdayTime('Vendredi', 'Midi').map((item, index) =>
+                  <Talk data={ item } key={index} />
+                ) }
+              </div>
+            </div>
+          </DayScheduleNavigation>
+
+          
+        </div>
+
+        <TalkDetailModal />
+
+      </div>
+    )
+  }
 }
 
 export default App;
