31f953aa7ebbb4091f8b76cdf59095f8aab3bb32
Ismaïl <nguyen.ismail@gmail.com>
2019-06-09 22:11:35 +0200

    Add roti express feedback iframe

--- src/Talk/TalkDetailModal.js
+++ src/Talk/TalkDetailModal.js
@@ -5,16 +5,18 @@ class TalkDetailModal extends React.Component {
     constructor(props) {
         super(props);
         this.state = { 
-            isBookmarked: this.isBookmarked()
+            showFeedbackForm: false
         };
     }
 
     getParsedData(key) {
         let talkData = this.props.talkData;
-        if (talkData)
-            return JSON.parse(talkData)[key]
         
-        return '';
+        if (!talkData) {
+            return '';
+        }
+        
+        return JSON.parse(talkData)[key] || '';
     }
 
     isBookmarked() {
@@ -33,7 +35,7 @@ class TalkDetailModal extends React.Component {
         return false;
     }
 
-    addToBookmark() {
+    addToBookmark () {
         let bookmarks = this.props.bookmarks;
         
         let talkData = this.props.talkData;
@@ -49,37 +51,71 @@ class TalkDetailModal extends React.Component {
                 bookmarks.push(currentTalkId)
             }
             
-            this.setState({ isBookmarked: !this.state.isBookmarked });
             localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
 
             this.props.bookmarkUpdated(true);
         }
     }
 
+    openFeedbackForm () {
+        this.setState({
+            showFeedbackForm: true
+        });
+    }
+
+    closeFeedbackForm () {
+        this.setState({
+            showFeedbackForm: false
+        });
+    }
+
   render() {
+    const feedbackUrl = this.getParsedData('feedbackUrl');
+    const isFeedbackUrlFilled = feedbackUrl !== '';
+    let feedbackButton;
+    if (isFeedbackUrlFilled) {
+        feedbackButton = <button type="button" onClick={() => this.openFeedbackForm() } className="btn btn-primary">Laisser un avis</button>
+    }
+
+    let bookmarkButton;
+    if (this.isBookmarked()) {
+        bookmarkButton = <button type="button" onClick={ () => this.addToBookmark() } className="btn btn-outline-danger float-right" data-dismiss="modal">Supprimer des favoris</button>;
+    } else {
+        bookmarkButton = <button type="button" onClick={ () => this.addToBookmark() } className="btn btn-danger float-right" data-dismiss="modal">Ajouter aux favoris</button>;
+    }
+
+    let talkLevel;
+    if (this.getParsedData('level') !== '') {
+        talkLevel = <div className="modal-footer">
+                        <span className="blockquote-footer">{ this.getParsedData('level') }</span>
+                    </div>;
+    }
+
     return (
         <div className="modal fade" id="talkDetailModal" tabIndex="-1" role="dialog" aria-hidden="true">
             <div className="modal-dialog modal-lg" role="document">
                 <div className="modal-content">
                     <div className="modal-header">
                         <h5 className="modal-title">{ this.getParsedData('title') }</h5>
-                        <button type="button" className="close" data-dismiss="modal" aria-label="Close">
+                        <button type="button" className="close" data-dismiss="modal" aria-label="Close" onClick={ () => this.closeFeedbackForm() }>
                             <span aria-hidden="true">&times;</span>
                         </button>
                     </div>
-                    <div className="modal-body" dangerouslySetInnerHTML={{__html: this.getParsedData('description')}}></div>
+                    { this.state.showFeedbackForm ?
+                        <div className="modal-body">
+                            <iframe title="Roti" src={ this.getParsedData('feedbackUrl') } scrolling="no" frameborder="0" style={{ border: "none", width: "100%", height: "500px" }}></iframe>
+                        </div>
+                        :
+                        <div className="modal-body" dangerouslySetInnerHTML={{__html: this.getParsedData('description')}}></div>
+                    }
+                    
+                    { talkLevel }
+                    
                     <div className="modal-footer">
-                        <span className="blockquote-footer">{ this.getParsedData('level') }</span>
-                    </div>
-                    <div className="modal-footer">
-                        {/* TODO: Activate when link with Roti.express works
-                            <button type="button" className="btn btn-primary">Laisser un avis</button> */}
+                        { feedbackButton }
                         
-                        { this.isBookmarked() ? 
-                            <button type="button" onClick={ () => this.addToBookmark() } className="btn btn-outline-danger float-right" data-dismiss="modal">Supprimer des favoris</button>
-                            :
-                            <button type="button" onClick={ () => this.addToBookmark() } className="btn btn-danger float-right" data-dismiss="modal">Ajouter aux favoris</button>
-                        }
+                        { bookmarkButton }
+
                     </div>
                 </div>
             </div>
